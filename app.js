e0db7f8712d59b9e175340ad04237aa0bf62701d
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-02-20 16:20:32 +0300

    task done

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 var secretNum = Math.ceil(Math.random() * 10);
 var tries = 0;
+console.log('Загадано число:', secretNum);
 
-function guessNum(num) {}
+function guessNum(num) {
+	if (tries == 5) return alert('Игра окончена');
+	if (secretNum === num) {
+		alert('Вы выиграли');
+		tries = 5;
+	} else {
+		alert(`Не угадали, загаданное число ${secretNum > num ? 'больше' : 'меньше'}`);
+		tries++;
+	}
+}
